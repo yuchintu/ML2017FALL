80457637009c8ed61701c8851d8e2db19bb6d986
yuchintu <r06922093@ntu.edu.tw>
2017-09-16 04:29:43 +0800

    modify Q1.py

--- hw0/Q1.py
+++ hw0/Q1.py
@@ -5,19 +5,23 @@ filein = open(sys.argv[1])
 #filein = open("words.txt")
 
 d = dict()
+l = []
 
 for line in filein:
     words = line.split();
     for word in words:
+        if(d.get(word, 0) == 0):
+            l.append(word)
         d[word] = d.get(word, 0) + 1
 
 output = open("Q1.txt","w")
 count = 0
 
-for word, times in d.items():
-    if(count < len(d) - 1):
-        output.write(str(word) + " " + str(count) + " "  + str(times) + "\n")
+for word in l:
+    if(count < len(l) - 1):
+        output.write(str(word) + " " + str(count) + " "  + str(d.get(word)) + "\n")
         count += 1
     else:
-        output.write(str(word) + " " + str(count) + " " + str(times))
+        output.write(str(word) + " " + str(count) + " " + str(d.get(word)))
 
+output.close()
